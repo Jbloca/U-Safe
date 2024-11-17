9c64d7ff45bef2afbd283099ada2aa2dc56dcd26
Jorge Balarezo <jbalarezocarden@gmail.com>
2024-11-17 15:45:44 -0500

    aa

---
+++ app/src/main/java/com/example/poofinal/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.poofinal;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn_login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnlogin = findViewById(R.id.btn_login);
+        btnlogin.setOnClickListener(view -> {
+            Intent login = new Intent(MainActivity.this, MenuView.class);
+            String usuario = ((EditText) findViewById(R.id.editTextText)).getText().toString();
+            String password = ((EditText) findViewById(R.id.editTextText2)).getText().toString();
+            if (usuario.equals("admin") && password.equals("admin"))
+            {
+                startActivity(login);
+            } else {
+                Toast.makeText(getApplicationContext(), "Usuario Incorrecto", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/poofinal/MenuView.java
@@ -0,0 +1,33 @@
+package com.example.poofinal;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageButton;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MenuView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_menu_view);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        ImageButton imageButton3 = findViewById(R.id.imageButton6);
+        imageButton3.setOnClickListener(view -> {
+            Intent intent = new Intent(MenuView.this, CodigoView.class);
+            startActivity(intent);
+        });
+    }
+}
