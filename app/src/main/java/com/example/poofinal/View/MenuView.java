3f32860161ac621b40e1d9136dfac3c4290254da
Dievalboza <diego.valdez@usil.pe>
2024-11-24 23:23:55 -0500

    MVC ConfigCode

--- app/src/main/java/com/example/poofinal/View/MenuView.java
+++ app/src/main/java/com/example/poofinal/View/MenuView.java
@@ -35,7 +35,7 @@ public class MenuView extends AppCompatActivity {
     private void navigateTo(Class<?> targetActivity) {
         Intent intent = new Intent(MenuView.this, targetActivity);
         startActivity(intent);
-        finish();
+
     }
 }
 
